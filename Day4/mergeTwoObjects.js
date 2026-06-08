4296f52d59494a969c481adb5be5e452011c7e6a
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-06-08 18:09:38 -0400

    feat(js): implement FizzBuzz solution

--- Day4/mergeTwoObjects.js
+++ Day4/mergeTwoObjects.js
@@ -7,3 +7,27 @@ Hint: Use the spread operator or Object.assign().
 
 
  */
+
+  function mergeObjects(obj1, obj2){
+   return {...obj1, ...obj2}
+  }
+
+
+  console.log(mergeObjects({a:1}, {b:2}));
+
+
+ /**
+  * Object.assign() JavaScript-এ একটি object-এর properties অন্য object-এ copy করার জন্য ব্যবহার করা হয়।
+Object.assign(target, source1, source2, ...)
+target → যেই object-এ data copy হবে
+source → যেখান থেকে data copy হবে
+  *  */ 
+
+
+function mergeObjects2(obj1, obj2){
+    let newObj = {}
+   return Object.assign(newObj, obj1, obj2)
+  }
+
+
+  console.log(mergeObjects2({a:1}, {b:2}));
